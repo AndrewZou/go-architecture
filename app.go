ab2a8e9c7bf7b9505ce60a5441d97319b9328a16
Andrew Zou <andrew.zou@shell.com>
2022-12-19 18:00:10 -0600

    Add unmarshal function.

--- app.go
+++ app.go
@@ -13,6 +13,7 @@ type person struct {
 
 func main() {
 	data_marshal()
+	data_unmarshal()
 }
 
 func data_marshal() {
@@ -34,5 +35,35 @@ func data_marshal() {
 	if err != nil {
 		log.Panic(err)
 	}
-	fmt.Println(string(bs))
+	fmt.Println("Marshaled data: ", string(bs))
+}
+
+func data_unmarshal() {
+	p1 := person{
+		First: "Andrew",
+		Last:  "Zou",
+	}
+
+	p2 := person{
+		First: "Wendy",
+		Last:  "Zou",
+	}
+
+	// create a slice/array of objects
+	xp := []person{p1, p2}
+
+	// Call json Marshal
+	bs, err := json.Marshal(xp)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// unMarshal the marshaled data
+	xp2 := []person{}
+
+	err2 := json.Unmarshal(bs, &xp2)
+	if err2 != nil {
+		log.Panic(err2)
+	}
+	fmt.Println("UnMarshal data: ", xp2)
 }
